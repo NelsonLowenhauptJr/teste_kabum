63e6815aa2aeebd11570e9ebccad2ef46912a1a7
Nelson Lowenhaupt Junior <52383645+NelsonLowenhauptJr@users.noreply.github.com>
2026-08-26 16:27:33 -0300

    chore: restore June 2022 working snapshot

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3
-ENV PYTHONBUFFERED=1
 WORKDIR /app
-COPY . .
+COPY requirements.txt /app/
 RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt
+COPY . /app/
--- app/database/database.py
+++ app/database/database.py
@@ -1,13 +1,20 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE="postgresql+psycopg2://postgres:password@localhost:5432/kabum_db"
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "kabum_db")
+POSTGRES_DB_HOST = os.getenv("POSTGRES_DB_HOST", "localhost")
 
-engine= create_engine(SQLALCHEMY_DATABASE)
-SessionLocal= sessionmaker(bind=engine)
+SQLALCHEMY_DATABASE = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_DB_HOST}:5432/{POSTGRES_DB}"
+
+engine = create_engine(SQLALCHEMY_DATABASE)
+SessionLocal = sessionmaker(bind=engine)
 
 def get_session_local():
 
-    session= SessionLocal()
+    session = SessionLocal()
        
     return session
